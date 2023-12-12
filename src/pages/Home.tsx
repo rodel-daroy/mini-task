2fc62ae924fffb44875a95da896ed665056870ed
rodel-daroy <rodeldaroy111@outlook.com>
2023-12-12 14:33:05 +0800

    mini-task

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,72 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonAvatar,
+  IonContent,
+  IonHeader,
+  IonItem, IonItemOption, IonItemOptions,
+  IonItemSliding,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar
+} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import React, {useEffect, useState} from "react";
+import {an} from "vitest/dist/reporters-5f784f42";
 
 const Home: React.FC = () => {
+
+  const [users, setUsers] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch('https://randomuser.me/api/?results=100'); // Replace with your API endpoint
+        if (response.ok) {
+          const jsonData = await response.json();
+          console.log('jsonData =====>', jsonData.results);
+          setUsers(jsonData.results);
+        } else {
+          throw new Error('Failed to fetch data');
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchUsers();
+  }, [])
+
+  const onDelete = (indexToRemove: number) => {
+    const updatedUsers = users.filter((_, index) => index !== indexToRemove);
+    setUsers(updatedUsers)
+  }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Mini Task</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <IonList>
+          {users.map((item, index) => (
+            <IonItemSliding key={index}>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <img src={item.picture.thumbnail} alt="" />
+                </IonAvatar>
+                <IonLabel>{`${item.name.first} ${item.name.last}`}</IonLabel>
+              </IonItem>
+
+              <IonItemOptions>
+                <IonItemOption color="danger" onClick={() => onDelete(index)}>Delete</IonItemOption>
+              </IonItemOptions>
+            </IonItemSliding>
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
